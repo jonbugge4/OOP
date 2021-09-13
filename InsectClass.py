aa8cfed44cb30a281b361fed540092d1cb72f411
Jonathan Bugge <jonathan_bugge1@baylor.edu>
2021-09-13 17:19:54 -0500

    Fix the issues on In class stuff

--- InsectClass.py
+++ InsectClass.py
@@ -7,7 +7,7 @@ class Insect:
         self.flight = 0
 
     def flight_length(self):
-        self.flight = random.int(1,10)
+        self.flight = random.randint(1,10)
 
     def get_miles(self):
         return self.flight
--- fly.py
+++ fly.py
@@ -8,4 +8,4 @@ mosquito.flight_length()
 housefly.flight_length()
 
 print('The mosquito can fly up to', mosquito.get_miles(), 'miles')
-print('the housefly can fly up to', housefly.get_miles(), ',miles')
+print('the housefly can fly up to', housefly.get_miles(), 'miles')
